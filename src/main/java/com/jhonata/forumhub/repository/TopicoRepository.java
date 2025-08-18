ffae20cd6265b359a4bd0ff61a642d25fcf7c4a1
jhonatadavid <jhonatadavidf@gmail.com>
2025-08-18 00:50:50 -0300

    Adiciona método buscar por id usando paginação.

--- src/main/java/com/jhonata/forumhub/repository/TopicoRepository.java
+++ src/main/java/com/jhonata/forumhub/repository/TopicoRepository.java
@@ -1,9 +1,12 @@
 package com.jhonata.forumhub.repository;
 
 import com.jhonata.forumhub.domain.entity.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    Page<Topico> findAll(Pageable pageable);
 }
