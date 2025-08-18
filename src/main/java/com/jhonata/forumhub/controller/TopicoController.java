3b336517f719a10849d412f5f139f1e34432d83e
jhonatadavid <jhonatadavidf@gmail.com>
2025-08-18 00:47:48 -0300

    Cria metodos listarTopicos e buscarTopicosPorId.

--- src/main/java/com/jhonata/forumhub/controller/TopicoController.java
+++ src/main/java/com/jhonata/forumhub/controller/TopicoController.java
@@ -1,12 +1,16 @@
 package com.jhonata.forumhub.controller;
 
 import com.jhonata.forumhub.domain.service.TopicoService;
+import com.jhonata.forumhub.dto.topico.TopicoRequestDTO;
 import com.jhonata.forumhub.dto.topico.TopicoResponseDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/topicos")
@@ -19,7 +23,23 @@ public class TopicoController {
     }
 
     @GetMapping()
-    public List<TopicoResponseDTO> getTopicos(){
-        return service.listarTopicos();
+    public Page<TopicoResponseDTO> listarTopicos(Pageable pageable){
+        return service.listarTopicos(pageable);
     }
+
+    @GetMapping("/topicos/{id}")
+    public ResponseEntity<TopicoResponseDTO> listarTopico(@PathVariable() Long id){
+        return service.buscarTopicoPorid(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> registrarTopico(@RequestBody @Valid TopicoRequestDTO topico){
+        service.salvarTopico(topico);
+        return ResponseEntity.ok().build();
+    }
+
+
+
 }
--- src/main/java/com/jhonata/forumhub/domain/service/TopicoService.java
+++ src/main/java/com/jhonata/forumhub/domain/service/TopicoService.java
@@ -1,40 +1,58 @@
 package com.jhonata.forumhub.domain.service;
 
-import com.jhonata.forumhub.dto.curso.CursoRequestDTO;
+import com.jhonata.forumhub.domain.entity.Curso;
+import com.jhonata.forumhub.domain.entity.Topico;
+import com.jhonata.forumhub.domain.enums.Categoria;
 import com.jhonata.forumhub.dto.topico.TopicoRequestDTO;
 import com.jhonata.forumhub.dto.topico.TopicoResponseDTO;
 import com.jhonata.forumhub.mapper.CursoMapper;
 import com.jhonata.forumhub.mapper.TopicoMapper;
+import com.jhonata.forumhub.repository.CursoRepository;
 import com.jhonata.forumhub.repository.TopicoRepository;
+import com.jhonata.forumhub.repository.UsuarioRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TopicoService {
 
     private final TopicoRepository repository;
+    private final CursoRepository cursoRepository;
     private final TopicoMapper mapper;
-    private final CursoMapper cursoMapper;
 
-    public TopicoService(TopicoRepository repository, TopicoMapper mapper, CursoMapper cursoMapper) {
+    public TopicoService(TopicoRepository repository, UsuarioRepository usuarioRepository, TopicoMapper mapper, CursoMapper cursoMapper, CursoRepository cursoRepository) {
         this.repository = repository;
         this.mapper = mapper;
-        this.cursoMapper = cursoMapper;
+        this.cursoRepository = cursoRepository;
     }
 
     @Transactional
-    public void salvarTopico(TopicoRequestDTO dto, CursoRequestDTO cursoRequestDTO) {
-        var curso = cursoMapper.dtoToEntity(cursoRequestDTO);
-        var novaEntity = mapper.dtoToEntity(dto, curso);
+    public void salvarTopico(TopicoRequestDTO dto) {
+        Curso curso = new Curso();
+        curso.setNome(dto.nomeCurso());
+        curso.setCategoria(Categoria.valueOf(dto.categoria()));
+        curso = cursoRepository.save(curso);
+
+        Topico novaEntity = mapper.dtoToEntity(dto, curso);
         this.repository.save(novaEntity);
     }
 
-    public List<TopicoResponseDTO> listarTopicos(){
-        return repository.findAll()
-                .stream()
-                .map(mapper::entityToDto)
-                .toList();
+    @Transactional(readOnly = true)
+    public Page<TopicoResponseDTO> listarTopicos(Pageable pageable) {
+        return repository.findAll(pageable)
+                .map(mapper::entityToDto);
+    }
+
+    public Optional<TopicoResponseDTO> buscarTopicoPorid(Long id){
+        return repository.findById(id).map(mapper::entityToDto);
+    }
+
+    @Transactional
+    public void atualizarTopico(TopicoRequestDTO topicoRequestDTO) {
+
     }
 }
