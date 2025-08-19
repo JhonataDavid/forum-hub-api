30ad2967d7880d24e0a0d23a5c16adc7337d71e0
jhonatadavid <jhonatadavidf@gmail.com>
2025-08-18 22:00:55 -0300

    Atualiza migrations

--- src/main/resources/db/migration/V3__create_table_topicos.sql
+++ src/main/resources/db/migration/V3__create_table_topicos.sql
@@ -3,8 +3,8 @@ CREATE TABLE topicos (
     titulo VARCHAR(255) NOT NULL,
     mensagem TEXT NOT NULL,
     data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    status_topico VARCHAR(255) NOT NULL,
-    usuario_id INT NOT NULL,
+    status_topico VARCHAR(255) NOT NULL DEFAULT 'ATIVO',
+    usuario_id INT,
     curso_id INT,
     CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
     CONSTRAINT fk_curso FOREIGN KEY (curso_id) REFERENCES cursos(id)
